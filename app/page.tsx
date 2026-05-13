214e35bdb15669a1af91927fe266f723a993ee32
intananggreini99 <d.intananggreini99@gmail.com>
2026-05-13 12:06:35 +0700

    Ganti text website

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Halo, Ini Project Vercel Saya!</h1>
-      <p className="mt-4">Berhasil di-deploy lewat Git.</p>
+      <h1 className="text-4xl font-bold">Assalamu'alaikum, Welcome to Project Vercel RiskFinder!</h1>
+      <p className="mt-4">Perkenalkan saya Intan Dwi Anggreini salah satu anggota RiskFinder Team.</p>
     </main>
   );
 }
